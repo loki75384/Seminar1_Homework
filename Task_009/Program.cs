62b92be9fcb5c7177a03127783ca8a7f813d14ec
loki47 <larsentuta49@gmail.com>
2023-06-15 16:49:00 +0300

    Задача на вытаскивание цифры из числа и их сравнение

---
+++ Task_009/Program.cs
@@ -0,0 +1,25 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Console.Clear();
+int number = new Random().Next(10, 100);
+int firstValue = number / 10;
+int secondValue = number % 10;
+Console.WriteLine(number + " " + firstValue + " " + secondValue);
+if (firstValue > secondValue)
+{
+    Console.WriteLine(firstValue);
+}
+else if (firstValue == secondValue)
+{
+    Console.WriteLine("Цифры числа равны");
+}
+else
+{
+    Console.WriteLine(secondValue);
+}
