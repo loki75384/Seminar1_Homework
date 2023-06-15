a0496ec7d00a725f04fe3449ced594db2880d642
loki47 <larsentuta49@gmail.com>
2023-06-15 20:18:23 +0300

    Задача про квадраты чисел, доделать

---
+++ Task_016/Program.cs
@@ -0,0 +1,22 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a * a == b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
