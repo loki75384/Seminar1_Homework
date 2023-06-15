5123e473afca3376293b9dc82dc7c51ad9d6be06
loki47 <larsentuta49@gmail.com>
2023-06-15 17:10:51 +0300

    Удаление второй цифры данного числа

---
+++ Task011/Program.cs
@@ -0,0 +1,14 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+int firstValue = number / 100;
+int secondValue = number % 10;
+int thirdValue = number % 10;
+Console.WriteLine(number + " " + firstValue + thirdValue);
+
