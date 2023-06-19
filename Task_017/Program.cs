dd1a0c65a0917aad31cd27f0fb688c27c933adf4
loki47 <larsentuta49@gmail.com>
2023-06-19 19:30:07 +0300

    Задача про координаты

---
+++ Task_017/Program.cs
@@ -0,0 +1,38 @@
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int x = Promt("Введите координату x: ");
+int y = Promt("Введите координату y: ");
+// Console.WriteLine();
+// Console.Write(x + " " + y);
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
