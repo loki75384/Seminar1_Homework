53ce9da73f992a1e8244b8b7cbc77f8d794f9311
loki47 <larsentuta49@gmail.com>
2023-06-23 11:48:39 +0300

    Task_023_Home

---
+++ Task_023_Home/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3-> 1, 8, 27
+// 5-> 1, 8, 27, 64, 125
+
+int number = Promt("Введите чмсло N: ");
+int i = 1;
+SqrtOfNumb(i, number);
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int number = Convert.ToInt32(value);
+    return number;
+}
+
+void SqrtOfNumb(int i, int number)
+{
+    while (i <= number)
+    {
+        Console.Write(i * i * i + " ");
+        i++;
+    }
+    return;
+}
